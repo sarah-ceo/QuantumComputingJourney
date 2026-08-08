df5ac6243e170dcde6f5513607c0a146d7e96ca1
Sarah CE-OUGNA <sarah.ceougna@gmail.com>
2026-08-08 16:01:41 +0200

    Updated Maze environment curriculum and reward systems

--- MazeEnvironment.py
+++ MazeEnvironment.py
@@ -48,7 +48,6 @@ class Maze(gym.Env):
         self._border_mask = np.zeros((self.screen_height, self.screen_width), dtype=bool)
         self._border_mask[:self._active_h, :self._active_w] = active_border_mask
 
-
     def compute_distance_map(self):
         dist = np.full((self.grid_size, self.grid_size), np.inf)
         exit_pos = tuple(np.argwhere(self.state == 1)[0])
@@ -77,43 +76,54 @@ class Maze(gym.Env):
         self.state[:, -1] = -1
 
         # Exit
-        exit_position = np.random.randint(1, self.grid_size-1, size=2)
+        exit_position = np.array([random.randint(1, self.grid_size-2), random.randint(1, self.grid_size-2)])
         self.state[exit_position[0], exit_position[1]] = 1
-
-        # Agent
-        self.agent_position = np.random.randint(1, self.grid_size-1, size=2)
-        while np.all(self.agent_position == exit_position):
-            self.agent_position = np.random.randint(1, self.grid_size-1, size=2)
-        self.state[self.agent_position[0], self.agent_position[1]] = 42
 
         # Inner walls
         if curriculum_level is None:
-            min_inner_walls, max_inner_walls, max_wall_length = 0, self.grid_size, self.grid_size
+            min_inner_walls, max_inner_walls, max_wall_length, max_agent_exit_dist = 0, self.grid_size, self.grid_size, self.grid_size
         else:
-            (min_inner_walls, max_inner_walls, max_wall_length) = curriculum_level
+            (min_inner_walls, max_inner_walls, max_wall_length, max_agent_exit_dist) = curriculum_level
         
+        # Agent
+        distance_matrix = np.maximum(
+            np.abs(np.arange(self.grid_size)[:, None] - exit_position[0]),
+            np.abs(np.arange(self.grid_size)[None, :] - exit_position[1])
+        ).astype(np.float32)
+        distance_matrix[np.where(self.state == -1)] = np.inf
+        possible_agent_positions = np.where((distance_matrix > 0) & (distance_matrix <= max_agent_exit_dist))
+        random_index = random.randint(0, len(possible_agent_positions[0])-1)
+        self.agent_position = np.array([possible_agent_positions[0][random_index], possible_agent_positions[1][random_index]])
+        self.state[*self.agent_position] = 42
+
+        # Inner walls
         n_inner_walls = random.randint(min_inner_walls, max_inner_walls)
         all_directions = np.array([[-1, 0], [1, 0], [0, -1], [0, 1]])
         diagonals = np.array([[-1, -1], [-1, 1], [1, -1], [1, 1]])
         for _ in range(n_inner_walls):
             walls = np.where(self.state == -1)
-            start_index = random.randint(0, len(walls[0])-1)
-            root = np.array([walls[0][start_index], walls[1][start_index]])
-            possible_directions = [d for d in all_directions if np.all(root+d >= 0) and np.all(root+d < self.grid_size) and np.all(self.state[(root+d)[0], (root+d)[1]] == 0)]
-            if len(possible_directions) > 0:
-                direction = random.choice(possible_directions)
-                row_or_col = np.where(direction != 0)[0]
-                look_ahead_directions = [d for d in np.vstack((all_directions, diagonals)) if d[row_or_col] != -1*direction[row_or_col]]
-                length = random.randint(2, max_wall_length)
-                for step in range(1, length+1):
-                    pos_delta = direction * step
-                    if self.state[(root+pos_delta)[0], (root+pos_delta)[1]] != 0:
-                        break
-                    if np.any(np.array([self.state[(root+pos_delta+d)[0], (root+pos_delta+d)[1]] for d in look_ahead_directions]) == -1):
-                        break
-                    self.state[(root+pos_delta)[0], (root+pos_delta)[1]] = -1
+            step = 1
+            attemps = 0
+            while step <= 1 and attemps<100:
+                attemps += 1
+                start_index = random.randint(0, len(walls[0])-1)
+                root = np.array([walls[0][start_index], walls[1][start_index]])
+                possible_directions = [d for d in all_directions if np.all(root+d >= 0) and np.all(root+d < self.grid_size) and np.all(self.state[(root+d)[0], (root+d)[1]] == 0)]
+                if len(possible_directions) > 0:
+                    direction = random.choice(possible_directions)
+                    row_or_col = np.where(direction != 0)[0]
+                    look_ahead_directions = [d for d in np.vstack((all_directions, diagonals)) if d[row_or_col] != -1*direction[row_or_col]]
+                    length = random.randint(2, max_wall_length)
+                    for step in range(1, length+1):
+                        pos_delta = direction * step
+                        if self.state[(root+pos_delta)[0], (root+pos_delta)[1]] != 0:
+                            break
+                        if np.any(np.array([self.state[(root+pos_delta+d)[0], (root+pos_delta+d)[1]] for d in look_ahead_directions]) == -1):
+                            break
+                        self.state[(root+pos_delta)[0], (root+pos_delta)[1]] = -1
 
         self.distance_map = self.compute_distance_map()
+        self.episode_max_steps = self.distance_map[*self.agent_position]
         return self.render_image(), {}
 
     def step(self, action: int):
@@ -127,7 +137,10 @@ class Maze(gym.Env):
         move = self.actions_correspondences[action]
         new_agent_position = self.agent_position + move
 
-        reward = self.state[*new_agent_position]
+        if self.steps > self.episode_max_steps:
+            reward = -1
+        else:
+            reward = self.state[*new_agent_position]
         truncated = self.steps >= self._max_episode_steps
         terminated = reward == -1 or reward == 1
 
@@ -145,7 +158,7 @@ class Maze(gym.Env):
             self.agent_position = new_agent_position
             self.state[*self.agent_position] = 42
 
-        return self.render_image(), reward*10.0, terminated, truncated, {}
+        return self.render_image(), reward, terminated, truncated, {}
     
     def create_image(self):
         color_grid = np.empty((self.grid_size, self.grid_size, 3), dtype=np.uint8)
@@ -174,27 +187,30 @@ if __name__ == "__main__":
     import matplotlib.pyplot as plt
     plt.ion()
 
+    curriculum_level = None
+
     seed = 42
     env = Maze()
-    env.reset(seed=seed, curriculum_level=None)
+    env.reset(seed=seed, curriculum_level=curriculum_level)
     env.action_space.seed(seed)
     env.observation_space.seed(seed)
 
     frame = env.render()
     img = plt.imshow(frame)
     plt.axis("off")
+
     for i in range(500):
         action = env.action_space.sample()
         _, reward, terminated, truncated, _ = env.step(action)
 
         if terminated or truncated:
-            env.reset(curriculum_level=None)
-        else:
-            frame = env.render()
-            img.set_data(frame)
-            
-            plt.draw()
-            plt.pause(0.1)
+            env.reset(curriculum_level=curriculum_level)
+
+        frame = env.render()
+        img.set_data(frame)
         
+        plt.draw()
+        plt.pause(5)
+
     plt.ioff()
     plt.show()
